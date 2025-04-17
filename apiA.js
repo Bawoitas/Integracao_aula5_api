049fb20cc33c0285d3f3cb7a440e6a45cf458042
Bawoitas <barbara.woitas@gmail.com>
2025-04-16 21:37:14 -0300

    redis

--- apiA.js
+++ apiA.js
@@ -1,34 +1,57 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 3000; 
+const port = 3000;
+const redis = require('redis');
+let client;
 
 app.get('/recommendation/:city', async (req, res) => {
-  const city = req.params.city;
+    const city = req.params.city;
 
-  try {
-    const response = await axios.get(`http://localhost:3001/weather/${city}`);
-    const temp = response.data.Temperatura;
+    try {
+        if (!client) {
+            client = redis.createClient({
+                host: 'localhost',
+                port: 6379
+            });
+            client.connect();
+        }
+        const cacheKey = `weather:${city}`;
+        let cachedData = await client.get(cacheKey);
+        cachedData = cachedData !== null ? JSON.parse(cachedData) : null;
 
-    let recommendation;
-    if (temp > 30) {
-      recommendation = 'Se hidrate e use protetor solar... no minimo fator 30';
-    } else if (temp >= 15 && temp <= 30) {
-      recommendation = 'O clima esta agradavel! So curticao em povao!';
-    } else {
-      recommendation = 'Leve um casaco! Se ta frio aqui, imagine em Curitiba';
+        if (!cachedData) {
+            cachedData = await axios.get(`http://localhost:3001/weather/${city}`);
+            client.set(
+                cacheKey,
+                JSON.stringify(cachedData.data),
+                {
+                    EX: 60
+                }
+            )
+        }
+
+        const temp = cachedData.data.Temperatura;
+
+        let recommendation;
+        if (temp > 30) {
+            recommendation = 'Se hidrate e use protetor solar... no minimo fator 30';
+        } else if (temp >= 15 && temp <= 30) {
+            recommendation = 'O clima esta agradavel! So curticao em povao!';
+        } else {
+            recommendation = 'Leve um casaco! Se ta frio aqui, imagine em Curitiba';
+        }
+
+        res.json({
+            Cidade: city,
+            Temperatura: temp,
+            Recomendamos: recommendation
+        });
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao consultar a API B' });
     }
-
-    res.json({
-      Cidade: city,
-      Temperatura: temp,
-      Recomendamos: recommendation
-    });
-  } catch (error) {
-    res.status(500).json({ error: 'Erro ao consultar a API B' });
-  }
 });
 
 app.listen(port, () => {
-  console.log(`API A esta rodando na porta ${port}`);
+    console.log(`API A esta rodando na porta ${port}`);
 });
